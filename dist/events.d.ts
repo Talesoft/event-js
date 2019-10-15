cd1ce08754b7fef8c40725ded95605807335d2df
Simone Kruegel <simone.kruegel04@gmail.com>
2019-10-15 18:53:10 +0100

    Initial Commit

--- dist/events.d.ts
+++ dist/events.d.ts
@@ -1,3 +1,5 @@
+export declare type EventConstructor<T extends Event> = new (...args: any[]) => T;
+export declare type EventListener<T extends Event> = (event: T) => void;
 declare const cancelledSymbol: unique symbol;
 declare const defaultPreventedSymbol: unique symbol;
 export declare abstract class Event {
@@ -8,12 +10,10 @@ export declare abstract class Event {
     cancel(): void;
     preventDefault(): void;
 }
-export declare type EventConstructor<T extends Event> = new (...args: any[]) => T;
-export declare type EventListener<T extends Event> = (event: T) => void;
 export declare class EventDispatcher {
     private listenerMap;
     addListener<T extends Event>(constructor: EventConstructor<T>, listener: EventListener<T>): void;
     removeListener<T extends Event>(constructor: EventConstructor<T>, listener: EventListener<T>): void;
-    dispatch<T extends Event>(event: T): boolean | undefined;
+    dispatch<T extends Event>(event: T): boolean;
 }
 export {};
